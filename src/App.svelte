e0667c741fbcead42ed3e6673dc4f8803b037fe6
Prathamesh Sakhadeo <pps@clickskills.in>
2023-04-26 15:33:44 +0530

    simple infobar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
+  import InfoBar from "./lib/components/InfoBar.svelte";
+
   let name = "Prathamesh";
 </script>
 
-<h1>Hello, {name}</h1>
+<div class="container w-5/6 mx-auto">
+  <h1>Hello, {name}</h1>
+  <InfoBar message="Hello, World!" shadow />
+  <InfoBar message="Hello, World!" type="success" />
+  <InfoBar message="Hello, World!" type="error" shadow />
+</div>
 
 <style lang="scss">
   h1 {
-    color: red;
+    @apply text-purple-700;
   }
 </style>
---
+++ src/lib/components/InfoBar.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  export let message: string;
+  export let type: string = "info";
+  export let shadow: boolean = false;
+</script>
+
+<div
+  class:error={type === "error"}
+  class:success={type === "success"}
+  class:info={type === "info" || !type}
+  class:shadow
+  class={`flex items-center justify-between border rounded gap-20 p-4 my-4 h-16`}
+>
+  <p>{message}</p>
+</div>
+
+<style lang="scss">
+  .info {
+    @apply bg-blue-100 text-blue-700 border-blue-700;
+  }
+
+  .success {
+    @apply bg-green-100 text-green-700 border-green-700;
+  }
+
+  .error {
+    @apply bg-red-100 text-red-700 border-red-700;
+  }
+
+  .shadow {
+    @apply shadow-xl;
+  }
+</style>
